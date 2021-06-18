ed0b787d5aba7cc1f9dc5fdc233bb337f3942c1b
Vivek Chandra <vivek.chandra@sixt.com>
2021-06-18 12:49:42 +0530

    Module federated button with hooks

--- joker/src/Button.js
+++ joker/src/Button.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React from 'react';
 
-const Button = () => <button>Joker has arrived</button>;
+const Button = () => {
+  const [count, setCount] = React.useState(0);
+  React.useEffect(() => console.log(count), [count]);
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>
+      Joker has arrived
+    </button>
+  );
+};
 
 export default Button;

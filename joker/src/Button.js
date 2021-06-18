32f896cee57a2264cbf29c19260f196468d6e79a
Vivek Chandra <vivek.chandra@sixt.com>
2021-06-18 13:23:42 +0530

    Working code with Class components

--- joker/src/Button.js
+++ joker/src/Button.js
@@ -1,5 +1,19 @@
 import React from 'react';
+export default class Button extends React.Component {
+  state = { counter: 1 };
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
 
+  render() {
+    return (
+      <button onClick={this.handleClick}>Click me: {this.state.counter}</button>
+    );
+  }
+}
+/*
 const Button = () => {
   const [count, setCount] = React.useState(0);
   React.useEffect(() => console.log(count), [count]);
@@ -11,3 +25,4 @@ const Button = () => {
 };
 
 export default Button;
+*/
